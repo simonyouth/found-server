a3a8d1e9420b5fc7d1a45b297f207246a5f0f624
simonyouth <2515240522@qq.com>
2019-03-29 18:27:54 +0800

    add some middleware

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const createError = require('http-errors');
 const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -14,17 +17,24 @@ app.set('view engine', 'pug');
 
 router(app);
 
+app.use();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // 设置静态文件
 app.use(express.static(path.join(__dirname, 'public')));
-
+// 通过适当地设置 HTTP 头，保护应用程序避免一些众所周知的 Web 漏洞。
+app.use(helmet());
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
 // });
+
+app.use(session({
+  resave: false, //
+
+}));
 
 app.use('/', (req, res) => {
   res.send('hello')
